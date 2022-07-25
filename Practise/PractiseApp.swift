69c0d72820e8b57fc43ce0148698437e4552340f
Abhiraj <abhirajbhattashali@gmail.com>
2022-07-26 02:24:16 +0530

    Initial Commit

---
+++ Practise/PractiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractiseApp.swift
+//  Practise
+//
+//  Created by Abhiraj on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PractiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
